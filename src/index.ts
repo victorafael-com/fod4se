42675e79717c3872d2c04f3bed124036b7e9e9dd
Victor Rafael <contato@victorafael.com>
2024-12-05 21:05:51 -0400

    add getTextBlocks to export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { LanguageFilter } from "./LanguageFilter";
 
-import { analyzeText, getSafeText } from "./Logic";
+import { analyzeText, getSafeText, getTextBlocks } from "./Logic";
 import { regexTemplate } from "./regexTemplates";
 
-export { LanguageFilter, analyzeText, getSafeText, regexTemplate };
+export {
+  LanguageFilter,
+  analyzeText,
+  getSafeText,
+  getTextBlocks,
+  regexTemplate,
+};
